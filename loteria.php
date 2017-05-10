b5f87102df99de6041c6f03e27d5f85dde3beb29
Nicholas Cervatti Duque Estrada <nicholas26cervatti@gmail.com>
2017-05-10 10:26:27 -0300

    Primeira pergunta. Inicio de tudo. O Alpha

--- loteria.php
+++ loteria.php
@@ -1,7 +1,10 @@
 <?php
 
-    echo "Quantas dezenas você quer ter? ";
-    $dezena = fgets(STDIN);
-    echo "Quantas apostas você quer fazer? ";
-    $aposta = fgets(STDIN);
 
+do {
+    print "1. Mega-Sena" . "\n" . "2. Quina" . "\n" . "3. Lotomania" . "\n" . "4. Lotofacil" . "\n" . "5. Sair"."\n"."Qual sera o jogo: ";
+
+    $jogo = trim(fgets(STDIN));
+
+
+}while($jogo > 6);
