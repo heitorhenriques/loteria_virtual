7718f3d4e564cef8998204a6f6bbd3d9e5d3fc4d
heitorhenriques <heiitorheenrique@gmail.com>
2017-05-10 11:22:27 -0300

    Sorteio de numeros
    funcao de sorteio de numeros e eles nao se reetirem, tem que deixar bonitinho depois

--- loteria.php
+++ loteria.php
@@ -1,10 +1,26 @@
 <?php
 
+    echo "Quantas dezenas você quer ter? ";
+    $dezena = 6;
+    echo "Quantas apostas você quer fazer? ";
+    $aposta = 2;
+function sorteio_numeros($dezenas){
+    $sorteado = [];
+    for ($i=1;$i<=$dezenas;$i++){
+        $sorteado[] = rand(1,60);
+    }
+    $tam = count($sorteado);
 
-do {
-    print "1. Mega-Sena" . "\n" . "2. Quina" . "\n" . "3. Lotomania" . "\n" . "4. Lotofacil" . "\n" . "5. Sair"."\n"."Qual sera o jogo: ";
+        for ($k = 0; $k < $tam; $k++) {
+            for ($j=$k+1;$j<$tam;$j++){
+            if ($sorteado[$k]==$sorteado[$j]) {
+                $sorteado[$k] = rand(1, 60);
+            }
+            }
+        }
 
-    $jogo = trim(fgets(STDIN));
-
-
-}while($jogo > 6);
+    return $sorteado;
+}
+for ($i=0;$i<$aposta;$i++){
+    print_r(sorteio_numeros($dezena));
+}
